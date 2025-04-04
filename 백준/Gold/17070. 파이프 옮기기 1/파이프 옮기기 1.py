00bf633bc1126e1fe9fc5c9e28588309aa564434
gsmin02 <gsmin5202@naver.com>
2025-04-04 14:09:53 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+house = [list(map(int, input().split())) for _ in range(N)]
+dp = [[[0] * 3 for _ in range(N)] for _ in range(N)]
+dp[0][1][0] = 1
+
+for i in range(N):
+    for j in range(2, N):
+        if house[i][j] == 0:
+            dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2]
+            if i > 0:
+                dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2]
+                if house[i-1][j] == 0 and house[i][j-1] == 0:
+                    dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2]
+
+result = (dp[N-1][N-1][0] + dp[N-1][N-1][1] + dp[N-1][N-1][2])
+print(result)
