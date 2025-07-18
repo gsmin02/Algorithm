b222ad2477d94b80c43c56a6f4febf91fbb5d6e0
gsmin02 <gsmin5202@naver.com>
2025-07-18 20:02:48 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.45 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -0,0 +1,10 @@
+def solution(A,B):
+    answer = 0
+    
+    sort_A = sorted(A)
+    sort_B = sorted(B, reverse=True)
+    
+    for i in range(len(A)):
+        answer += sort_A[i] * sort_B[i]
+
+    return answer
