da6ce5a9182f7746077f673795da2fd728ec41a5
gsmin02 <gsmin5202@naver.com>
2026-02-14 02:02:57 +0900

    [level 2] Title: 전화번호 목록, Time: 434.31 ms, Memory: 45.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,12 +1,22 @@
+"""
+[문제]
+입력된 번호가 다른 번호의 접두어인 경우 false 아니면 true를 반환
+
+[해석]
+전화번호를 가져와 해당 번호로 시작하는 다른 번호가 있는지 확인
+
+[풀이]
+전화번호 추출 후 문자열을 더해서 원본과 비교
+> 본인과 같은 경우는 제외
+"""
 def solution(phone_book):
-    cnt = 0
-    dic = dict()
-    for i in range(len(phone_book)):
-        dic[phone_book[i]] = i
-    for i in range(len(phone_book)):
-        for j in range(len(phone_book[i])):
-            cnt += 1
-            if phone_book[i][0:j] in dic:
-                print(cnt)
+    hash_phone = {}
+    for phone_number in phone_book:
+        hash_phone[phone_number] = True
+    for phone_number in phone_book:
+        diff = ""
+        for number in phone_number:
+            diff += number
+            if diff in hash_phone and diff != phone_number:
                 return False
     return True
