e70683904adbeef8e29267dd98163f75b5556c86
gsmin02 <gsmin5202@naver.com>
2026-03-10 16:06:08 +0900

    [Bronze V] Title: ??!, Time: 76 ms, Memory: 12344 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.kt
+++ 백준/Bronze/10926. ？？！/？？！.kt
@@ -1,12 +1,6 @@
 import java.util.Scanner
 
-fun solution(): String {
-	val input = readln()
-	val answer = "$input??!"
-	
-	return answer
-}
-
 fun main() {
-	println(solution())
+	val input = readln()
+	println("$input??!")
 }
